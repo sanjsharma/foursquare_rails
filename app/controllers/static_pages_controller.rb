cfd8bc9235a23679ca2fa1920ddbd6b618ad8843
Sanjay Sharma <sanjay@ekbana.com>
2015-08-05 16:49:25 +0545

    integrated foursquare api to track venue history

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,8 @@
 class StaticPagesController < ApplicationController
   def welcome
+  	if current_user and current_user.token
+	  	client = Foursquare2::Client.new(:oauth_token => current_user.token, :api_version => '20120609')
+	  	@venue_history = client.user_venue_history(options = {})
+	  end
   end
 end
